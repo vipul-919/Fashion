177c64bbd0323f3924db07f8cfad6b4075dbdec2
vipul-919 <monkeyd.luffy000919@gmail.com>
2024-12-09 17:19:09 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const banner = document.querySelector(".banner__container");
+
+const bannerContent = Array.from(banner.children);
+
+bannerContent.forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  banner.appendChild(duplicateNode);
+});
