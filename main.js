6ccf76df3f7b6a295dcb4acc58d9815f132b9f3c
vipul-919 <monkeyd.luffy000919@gmail.com>
2024-12-09 20:41:09 +0530

    upload

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+//navbar//
+// Select elements
+const menuBtn = document.getElementById("menu_btn");
+const navLinks = document.getElementById("nav_link");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+// Toggle the menu
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+// Close the menu when a link is clicked
+navLinks.addEventListener("click", (e) => {
+    if (e.target.tagName === "A" || e.target.tagName === "BUTTON") {
+        navLinks.classList.remove("open");
+        menuBtnIcon.setAttribute("class", "ri-menu-line");
+    }
+});
+
+//banner//
 const banner = document.querySelector(".banner__container");
 
 const bannerContent = Array.from(banner.children);
